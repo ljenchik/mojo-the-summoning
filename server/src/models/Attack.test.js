a85152861d4e95a7669dacc4c5a561ede14852ed
ljenchik <pelagenkolena@gmail.com>
2023-11-03 16:19:43 +0000

    Added CRUD tests for attack model

--- server/src/models/Attack.test.js
+++ server/src/models/Attack.test.js
@@ -10,25 +10,22 @@ const { db } = require("../db/config");
 //require("./../db/seed");
 const { seed } = require("../db/seed");
 
-// define in global scope
-let attack;
-
 // clear db and create new user before tests
 beforeAll(async () => {
     await db.sync({ force: true });
     await seed();
-    attack = await Attack.create({
-        title: "power",
-        mojoCost: 20,
-        staminaCost: 10,
-    });
 });
 
 // clear db after tests
 //afterAll(async () => await db.sync({ force: true }));
 
 describe("Attack", () => {
-    test("has an id", async () => {
+    test("creates attack", async () => {
+        const attack = await Attack.create({
+            title: "power",
+            mojoCost: 20,
+            staminaCost: 10,
+        });
         expect(attack).toHaveProperty("id");
         expect(attack.title).toBe("power");
         expect(typeof attack.title).toBe("string");
@@ -38,6 +35,36 @@ describe("Attack", () => {
         expect(typeof attack.staminaCost).toBe("number");
         expect(attack instanceof Attack).toBeTruthy();
     });
+
+    test("reads attack", async () => {
+        const foundAttack = await Attack.findByPk(2);
+        expect(foundAttack).toHaveProperty("id");
+        expect(typeof foundAttack.title).toBe("string");
+        expect(foundAttack.title).toBe("hurt");
+        expect(foundAttack instanceof Attack).toBeTruthy();
+    });
+
+    test("updates correct attack", async () => {
+        const foundAttack = await Attack.findByPk(2);
+        await foundAttack.update({ title: "jump" });
+        expect(foundAttack.title).toBe("jump");
+        expect(typeof foundAttack.title).toBe("string");
+        expect(foundAttack instanceof Attack).toBeTruthy();
+        const allAttacks = await Attack.findAll();
+        expect(allAttacks).toEqual(
+            expect.arrayContaining([expect.objectContaining({ title: "jump" })])
+        );
+    });
+
+    test("deletes correct attack", async () => {
+        const allAttacks = await Attack.findAll();
+        const length = allAttacks.length;
+        const destroyedAttack = await Attack.findByPk(3);
+        await destroyedAttack.destroy();
+        const newAllAttacks = await Attack.findAll();
+        expect(newAllAttacks.length).toBe(length - 1);
+    });
+
     test("attack belongs to many cards", async () => {
         const attack = await Attack.findByPk(1);
         const card1 = await Card.findByPk(1);
