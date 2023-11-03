ec17a65404b3f293be0c8fd566406cbfed69fb60
ljenchik <pelagenkolena@gmail.com>
2023-11-03 10:17:33 +0000

    Created associations between models

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -4,5 +4,16 @@ const Card = require("./Card");
 const Attack = require("./Attack");
 
 //set up the associations here
+// 1:1 for User-Deck association
+User.hasOne(Deck);
+Deck.belongsTo(User);
+
+// 1:many for Deck-Card association
+Deck.hasMany(Card);
+Card.belongsTo(Deck);
+
+//many:many
+Card.belongsToMany(Attack, { through: "card-attack" });
+Attack.belongsToMany(Card, { through: "card-attack" });
 
 module.exports = { User, Deck, Card, Attack };
